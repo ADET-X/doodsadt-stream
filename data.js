5b6bb11c47f9382d4d143d691a1cca4f493bc068
ADET-X <107465531+ADET-X@users.noreply.github.com>
2025-09-09 06:35:42 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,15 @@
+// data.js
+// Isi hanya ID, title dan durasi. Thumbnail akan dibuat otomatis dari:
+// https://dsimgcdn.com/splash/<ID>.jpg
 const videos = [
+  { id: "fz3za1fmorz3", title: "Video 1", duration: "10:00" },
+  { id: "nwzub371aesr", title: "Video 2", duration: "12:15" },
+  { id: "a09z7rdx4i7u", title: "Video 3", duration: "08:30" },
+  { id: "6fiuf0c4s0nt", title: "Video 4", duration: "09:45" },
+  { id: "nbirf772l1kz", title: "Video 5", duration: "11:20" },
+  { id: "uln76u4530uq", title: "Video 6", duration: "07:50" },
+  { id: "o7kn6la4mazi", title: "Video 7", duration: "13:10" }
+];const videos = [
   {
     id: "fz3za1fmorz3",
     title: "Video 1",
