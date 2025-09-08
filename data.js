e8073d3910e2c5ee980ff6a910ff019e92582c64
ADET-X <107465531+ADET-X@users.noreply.github.com>
2025-09-09 06:47:59 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,48 +1,107 @@
-// data.js
-// Isi hanya ID, title dan durasi. Thumbnail akan dibuat otomatis dari:
-// https://dsimgcdn.com/splash/<ID>.jpg
 const videos = [
-  { id: "fz3za1fmorz3", title: "Video 1", duration: "10:00" },
-  { id: "nwzub371aesr", title: "Video 2", duration: "12:15" },
-  { id: "a09z7rdx4i7u", title: "Video 3", duration: "08:30" },
-  { id: "6fiuf0c4s0nt", title: "Video 4", duration: "09:45" },
-  { id: "nbirf772l1kz", title: "Video 5", duration: "11:20" },
-  { id: "uln76u4530uq", title: "Video 6", duration: "07:50" },
-  { id: "o7kn6la4mazi", title: "Video 7", duration: "13:10" }
-];const videos = [
   {
-    id: "fz3za1fmorz3",
+    id: "tur2drxevxke",
     title: "Video 1",
-    duration: "10:00"
+    duration: "10:00",
+    thumbnail: "https://dsimgcdn.com/splash/tur2drxevxke.jpg",
+    link: "https://doodstream.com/e/tur2drxevxke"
   },
   {
-    id: "nwzub371aesr",
+    id: "edo91eliqzj9",
     title: "Video 2",
-    duration: "12:15"
+    duration: "12:30",
+    thumbnail: "https://dsimgcdn.com/splash/edo91eliqzj9.jpg",
+    link: "https://doodstream.com/e/edo91eliqzj9"
   },
   {
-    id: "a09z7rdx4i7u",
+    id: "464jw7dydnoe",
     title: "Video 3",
-    duration: "08:30"
+    duration: "09:45",
+    thumbnail: "https://dsimgcdn.com/splash/464jw7dydnoe.jpg",
+    link: "https://doodstream.com/e/464jw7dydnoe"
   },
   {
-    id: "6fiuf0c4s0nt",
+    id: "jzsaxk0e4aod",
     title: "Video 4",
-    duration: "09:45"
+    duration: "11:20",
+    thumbnail: "https://dsimgcdn.com/splash/jzsaxk0e4aod.jpg",
+    link: "https://doodstream.com/e/jzsaxk0e4aod"
   },
   {
-    id: "nbirf772l1kz",
+    id: "ukfad3vzpxcb",
     title: "Video 5",
-    duration: "11:20"
+    duration: "08:15",
+    thumbnail: "https://dsimgcdn.com/splash/ukfad3vzpxcb.jpg",
+    link: "https://doodstream.com/e/ukfad3vzpxcb"
   },
   {
-    id: "uln76u4530uq",
+    id: "1p3xxjd3nv9w",
     title: "Video 6",
-    duration: "07:50"
+    duration: "14:00",
+    thumbnail: "https://dsimgcdn.com/splash/1p3xxjd3nv9w.jpg",
+    link: "https://doodstream.com/e/1p3xxjd3nv9w"
   },
   {
-    id: "o7kn6la4mazi",
+    id: "td72eys6lnl6",
     title: "Video 7",
-    duration: "13:10"
+    duration: "07:50",
+    thumbnail: "https://dsimgcdn.com/splash/td72eys6lnl6.jpg",
+    link: "https://doodstream.com/e/td72eys6lnl6"
+  },
+  {
+    id: "aknhdsxxwk12",
+    title: "Video 8",
+    duration: "13:40",
+    thumbnail: "https://dsimgcdn.com/splash/aknhdsxxwk12.jpg",
+    link: "https://doodstream.com/e/aknhdsxxwk12"
+  },
+  {
+    id: "4oqyv0ljb8jd",
+    title: "Video 9",
+    duration: "09:10",
+    thumbnail: "https://dsimgcdn.com/splash/4oqyv0ljb8jd.jpg",
+    link: "https://doodstream.com/e/4oqyv0ljb8jd"
+  },
+  {
+    id: "wrpopy8lg85x",
+    title: "Video 10",
+    duration: "15:25",
+    thumbnail: "https://dsimgcdn.com/splash/wrpopy8lg85x.jpg",
+    link: "https://doodstream.com/e/wrpopy8lg85x"
+  },
+  {
+    id: "aqbbiytfrhu4",
+    title: "Video 11",
+    duration: "06:55",
+    thumbnail: "https://dsimgcdn.com/splash/aqbbiytfrhu4.jpg",
+    link: "https://doodstream.com/e/aqbbiytfrhu4"
+  },
+  {
+    id: "mb3kqudt2ob0",
+    title: "Video 12",
+    duration: "11:45",
+    thumbnail: "https://dsimgcdn.com/splash/mb3kqudt2ob0.jpg",
+    link: "https://doodstream.com/e/mb3kqudt2ob0"
+  },
+  {
+    id: "sgmqk1owo2k6",
+    title: "Video 13",
+    duration: "10:30",
+    thumbnail: "https://dsimgcdn.com/splash/sgmqk1owo2k6.jpg",
+    link: "https://doodstream.com/e/sgmqk1owo2k6"
+  },
+  {
+    id: "rallzsa48blh",
+    title: "Video 14",
+    duration: "12:10",
+    thumbnail: "https://dsimgcdn.com/splash/rallzsa48blh.jpg",
+    link: "https://doodstream.com/e/rallzsa48blh"
+  },
+  {
+    id: "7sbnqxod4jrg",
+    title: "Video 15",
+    duration: "09:20",
+    thumbnail: "https://dsimgcdn.com/splash/7sbnqxod4jrg.jpg",
+    link: "https://doodstream.com/e/7sbnqxod4jrg"
   }
 ];
